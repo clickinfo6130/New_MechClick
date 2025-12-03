0f88fd87556b07cfe7ece08c7f68879f70483018
BYKang <rkdqhdud1108@clickinfo.co.kr>
2025-12-03 15:51:33 +0900

    최종본 커밋

---
+++ BoltSpecProgram/BoltSpecProgram/Services/IpcMessage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common.Protocol
+{
+    public abstract class IpcMessage
+    {
+        public string MessageType { get; set; }
+        public string MessageId { get; set; } = Guid.NewGuid().ToString();
+    }
+
+    public class ShowDialogRequest : IpcMessage
+    {
+        public ShowDialogRequest()
+        {
+            MessageType = "ShowDialog";
+        }
+
+        public string DialogType { get; set; }
+        public Dictionary<string, object> Context { get; set; }
+    }
+
+    public class DialogResponse : IpcMessage
+    {
+        public DialogResponse()
+        {
+            MessageType = "DialogResponse";
+        }
+
+        public bool IsOk { get; set; }
+        public Dictionary<string, object> SelectedData { get; set; }
+    }
+}
